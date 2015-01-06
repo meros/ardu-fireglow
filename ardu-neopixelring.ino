417a7e838c276b38c74159afc2e0188d3d02f075
Alexander Schrab <alexander.schrab@gmail.com>
2015-01-06 13:17:05 +0100

    Update ardu-neopixelring.ino
    Made a simple loop time independent animation class

--- ardu-neopixelring.ino
+++ ardu-neopixelring.ino
@@ -7,18 +7,68 @@ Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ80
 
 int delayval = 500;
 
+class Color {
+private:
+  uint8_t r;
+  uint8_t g;
+  uint8_t b;
+public:
+  Color(uint8_t r, uint8_t g, uint8_t b) :r(r), g(g), b(b) {}
+  
+  static Color mix(Color& a, Color& b, uint8_t scale) {
+    return Color(
+      map(scale, 0, 255, a.r, b.r),
+      map(scale, 0, 255, a.g, b.g),
+      map(scale, 0, 255, a.b, b.b));
+  }
+  
+  uint32_t getColor() {
+    return Adafruit_NeoPixel::Color(r, g, b);
+  }
+};
+
+class Fade {
+private:
+  int startMillis;
+  int durationMillis;
+  
+  Color from;
+  Color to;
+  
+public:
+  Fade(int startMillis, int durationMillis, Color from, Color to) :
+  startMillis(startMillis),
+  durationMillis(durationMillis),
+  from(from),
+  to(to) {}
+  
+  void render(int nowMillis, Adafruit_NeoPixel& pixels) {    
+    uint32_t color = Color::mix(from, to, map(nowMillis, startMillis, startMillis+durationMillis, 0, 255)).getColor();
+    
+    for (int i = 0; i < pixels.numPixels(); i++) {
+      pixels.setPixelColor(i, color);
+    }
+  }
+  
+  boolean done(int nowMillis) {
+    return nowMillis > startMillis + durationMillis;
+  }
+};
+
+Fade* currentFade = NULL;
+
 void setup() {
   pixels.begin(); 
 }
 
 void loop() {
-  for(int i=0;i<NUMPIXELS;i++){
-    pixels.setPixelColor(i, 
-      pixels.Color(
-        map(i, 0, NUMPIXELS, 0, 255),
-        map(i, 0, NUMPIXELS, 255, 0),
-        0));
-    pixels.show(); 
-    delay(delayval); 
+  int now = millis();
+  
+  if (currentFade == NULL || currentFade->done(now)) {
+    delete(currentFade);
+    currentFade = new Fade(millis(), 1000, Color(0,0,255), Color(255,0,0));
   }
+  
+  currentFade->render(now, pixels);
+  pixels.show();
 }
